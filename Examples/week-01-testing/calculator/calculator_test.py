3634fda2c20d8ff4819aed83d46e461a7dce3f43
rajban1918 <rjalbright@gmail.com>
2015-04-14 18:13:08 -0700

    week 1 changes

--- Examples/week-01-testing/calculator/calculator_test.py
+++ Examples/week-01-testing/calculator/calculator_test.py
@@ -11,6 +11,14 @@ class TestCalculatorFunctions(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(self.x, self.y), 5)
 
+    def test_subtract_neg(self):
+        self.assertEqual(calc.subtract(self.x, self.y), -1)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(self.x, self.y), 6)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(self.y, self.x), 1.5)
 
 if __name__ == "__main__":
     unittest.main()
--- Examples/week-01-testing/context_manager/exercise1.py
+++ Examples/week-01-testing/context_manager/exercise1.py
@@ -1,3 +1,13 @@
+
+class YourExceptionHandler(object):
+
+    def __init__(self):
+        pass
+    def __enter__(self):
+        pass
+    def __exit__(self, type, value, tb):
+        return True
+
 with YourExceptionHandler():
     print "do some stuff here"
     1/0
